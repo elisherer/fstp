4a8ca381de6b415470db9f2273690394954d1a30
Eli Sherer <eli.sherer@gmail.com>
2020-10-20 18:05:19 +0300

    fix cors tests

--- src/modules/__tests__/cors.test.js
+++ src/modules/__tests__/cors.test.js
@@ -1,11 +1,24 @@
 const cors = require("../cors");
 
 test("set headers", () => {
-  const ctx = { res: { setHeader: jest.fn() } };
+  const ctx = { req: {}, res: { setHeader: jest.fn() } };
   cors(ctx);
   expect(ctx.res.setHeader).toHaveBeenCalledTimes(3);
   expect(ctx.res.setHeader).toHaveBeenCalledWith(
     expect.stringMatching(/^Access-Control-Allow-[A-Z][a-z]+$/),
     expect.any(String)
   );
+});
+
+test("answer options", () => {
+  const ctx = { req: { method: 'OPTIONS' }, res: { setHeader: jest.fn(), end: jest.fn() } };
+  cors(ctx);
+  expect(ctx.res.setHeader).toHaveBeenCalledTimes(3);
+  expect(ctx.res.setHeader).toHaveBeenCalledWith(
+    expect.stringMatching(/^Access-Control-Allow-[A-Z][a-z]+$/),
+    expect.any(String)
+  );
+  expect(ctx.res.statusCode).toBe(200);
+  expect(ctx.res.end).toHaveBeenCalledTimes(1);
+  expect(ctx.res.end).toHaveBeenCalledWith();
 });
